00b6ae3e9b118f2355ec760fd5d732971fafed4b
zhihuahuang <zhihuahuang.email@gmail.com>
2014-07-31 21:27:53 +0800

    Modify MD5 Class and Add BaseCoversion Class

--- src/code/java/digest/MD5.java
+++ src/code/java/digest/MD5.java
@@ -6,6 +6,8 @@ package code.java.digest;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import code.java.util.BaseConversion;
+
 public class MD5 {
 	private static MessageDigest messageDigest;
 	private static NoSuchAlgorithmException exception;
@@ -18,11 +20,27 @@ public class MD5 {
 		}
 	}
 
-	public static byte[] hash(byte[] input) throws NoSuchAlgorithmException {
+	/**
+	 * Generate a byte array by MD5.
+	 * @param input is a byte array
+	 * @return A byte array
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static byte[] generate(byte[] input) throws NoSuchAlgorithmException {
 		if(exception != null)
 			throw exception;
 		
 		messageDigest.update(input);
 		return messageDigest.digest(); //MD5
+	}
+	
+	/**
+	 * Generate a lower case hex string by MD5.
+	 * @param input is a byte array
+	 * @return A lower case hex string
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String generateHexString(byte[] input) throws NoSuchAlgorithmException{
+		return BaseConversion.bytesToHexString(generate(input));
 	}
 }
